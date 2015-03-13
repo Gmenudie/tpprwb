b3a97017b8f08c8e34d958fd9f9c591965015c52
Geoffray <menuay@hotmail.fr>
2015-03-13 09:33:46 +0100

    Basic introduction to routes and views

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,18 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return status(488,	"Strange response type");	
     }
+
+     public static Result show() {
+        return ok(show.render("This is sparta! (and my application also)", "(BTE) Best Title Ever"));	
+    }
+    public static Result display() {
+        return ok(show.render("This is the display page!(almost same as show but different)", "(BDTE) Best Display Title Ever"));	
+    }
+
+   /* public	static Result hello(String name)	{	
+		return redirect(controllers.routes.Application.show(name+" is tested"));	
+	}*/	
 
 }
